eaf2f5e49cd76b569c4f219246020b9c3344e938
Jacob <jacobmeide@gmail.com>
2023-08-01 07:56:58 -0500

    Add new Page in Generator App

--- generator/urls.py
+++ generator/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import *
+
+app_name = 'generator'
+
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('project/', Project.as_view(), name='project')
 ]
--- generator/views.py
+++ generator/views.py
@@ -4,3 +4,6 @@ from django.views.generic.base import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+class Project(TemplateView):
+    template_name = "project.html"
